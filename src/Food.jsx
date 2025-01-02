fd731e0582d12bb7777662c05feff99c5f8cbd08
siddPandey <pd.siddharth@gmail.com>
2025-01-02 13:36:30 -0500

    adding onclick event to the button

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Food(propx) {
+function Food({ brand }) { // Destructure props and rename to 'props'
     const food1 = "Apple";
     const food2 = "Orange";
 
     return (
         <ul>
             <li>{food1}</li>
-            <li>Mango {propx.brand}</li>
+            <li>Mango {brand}</li> {/* Use destructured 'brand' */}
             <li>{food2.toUpperCase()}</li>
         </ul>
     );

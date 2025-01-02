bf14fa6783afab17591f95347b5071dc0d7cb6ec
siddPandey <pd.siddharth@gmail.com>
2025-01-02 13:40:37 -0500

    code updated in ffod for onclick

--- src/Food.jsx
+++ src/Food.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 function Food({ brand }) { // Destructure props and rename to 'props'
     const food1 = "Apple";
     const food2 = "Orange";
-
+    const shoot = () => {
+        
+        alert("Great Shot!");
+      }
     return (
         <ul>
-            <li>{food1}</li>
+            <li><a onClick={shoot}>{food1}</a></li>
             <li>Mango {brand}</li> {/* Use destructured 'brand' */}
             <li>{food2.toUpperCase()}</li>
         </ul>
